eac13ecc02c9a2715fc025c23f21cfb8bcbaedf3
shikhasingh4934 <shikhasingh4934@gmail.com>
2024-09-13 00:39:58 -0400

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import openai
 
 first_page = st.Page("lab1.py",title="first page")
 second_page = st.Page("lab2.py",title="second page")
-Third_page = st.Page("lab3.py",title="Third page")
+Third_page = st.Page("lab3A.py",title="Third page")
 
 pg = st.navigation([first_page,second_page,Third_page])
 st.set_page_config(page_title="test")
