8fb877e0ac8788b63eeeae97de14e754f6b8cb3e
shikhasingh4934 <shikhasingh4934@gmail.com>
2024-09-25 23:30:03 -0400

    new

--- lab4.py
+++ lab4.py
@@ -4,7 +4,7 @@ import chromadb
 from PyPDF2 import PdfReader
 import os
 
-# Ensure the API key is set
+# Ensure the API key 
 openai.api_key = st.secrets["API_KEY"]
 
 # Function to generate embeddings for a given text
