eafd0a7124fd401037329eb178b9f643fa76b45a
shikhasingh4934 <shikhasingh4934@gmail.com>
2024-09-25 23:41:48 -0400

    MAIN

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import openai
+import openai 
 
 
-first_page = st.Page("lab1.py",title="first page")
+first_page = st.Page("lab1.py",title="First page")
 second_page = st.Page("lab2.py",title="second page")
 Third_page = st.Page("lab3A.py",title="Third page")
 Fourth_page = st.Page("lab4.py",title="Fourth page")
